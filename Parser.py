e85aedd354ef775424da73c73303b5b176eb2976
Gavin Lai <gavin09@gmail.com>
2014-05-23 01:56:56 +0800

    Remove Parser openfile function, it is for debug in original

--- Parser.py
+++ Parser.py
@@ -8,20 +8,13 @@ class Parser:
    def __init__(self):
       self.debug = 0
 
-   def openFile(self):
-      reader = open('dump.txt', 'r')
-      return reader.read()
-
    def Article(self, message):
-      if self.debug is 1:
-         message = self.openFile()
-
       if self.debug is 1:
          print repr(message)
 
       # \xe2\x97\x8f is ●
       # 3H is the position of article id
-      lines = re.split('3H|\\xe2\\x97\\x8f|\\r\\n ', message)
+      lines = re.split('3H|\\xe2\\x97\\x8f|\\r\\n', message)
 
       for line in lines:
 
