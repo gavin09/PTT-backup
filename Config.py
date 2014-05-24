b279b4dcdc4cc5b56d3c204218e0b0739573985e
Gavin <gavin@gavin-sin1.(none)>
2014-05-24 17:13:01 +0000

    Support parse account and password

--- Config.py
+++ Config.py
@@ -2,25 +2,37 @@ import ConfigParser
 import sys
 import re
 
-def getRule():
-   config = ConfigParser.ConfigParser()
-   config.optionform = str
-   config.read('config.ini')
-   return config
+class ConfigReader:
+   def __init__(self, filename):
+      self.config = ConfigParser.ConfigParser()
+      self.config.optionform = str
+      self.config.read(filename)
 
-def getBoardFromFile(filename):
-   f = open(filename, 'r')
-   boards = f.readlines()
-   f.close()
-   return boards
+   def getAccount(self):
+      return self.config.get('Account', 'account')
 
-def getRuleFromFile(filename):
-   f = open(filename, 'r')
-   rules = f.readlines()
+   def getPassword(self):
+      return self.config.get('Account', 'password')
 
-   for i in range(len(rules)):
-      rules[i] = re.sub('\n', '', rules[i])
-   f.close()
-   return rules
+   def getRule(self):
+      config = ConfigParser.ConfigParser()
+      config.optionform = str
+      config.read('config.ini')
+      return config
+   
+   def getBoardFromFile(self, filename):
+      f = open(filename, 'r')
+      boards = f.readlines()
+      f.close()
+      return boards
+   
+   def getRuleFromFile(self, filename):
+      f = open(filename, 'r')
+      rules = f.readlines()
+   
+      for i in range(len(rules)):
+         rules[i] = re.sub('\n', '', rules[i])
+      f.close()
+      return rules
 
 
